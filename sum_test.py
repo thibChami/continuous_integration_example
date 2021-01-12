67a85ef83a4a8b9551c80aeca39d5beedf913957
dontcrenie <dontcrenie@yahoo.fr>
2021-01-12 20:23:02 +0100

    Exercice

--- sum_test.py
+++ sum_test.py
@@ -1,19 +1,12 @@
 from sum import mysum
-
-def test_sum_integers():
-    assert mysum([0, 1, 2, 3, 4]) == 10
-def test_sum_floats():
-    assert mysum([0.1, 1.2, 2.3, 3.4, 4.5]) == 11.5
-def test_sum_nothing():
-    assert mysum([]) == 0
-
-
 import pytest
 
 
 @pytest.mark.parametrize('numbers, output', [
     ([], 0),
     ([10, 20, 30], 60),
-    ([0.1, 1.2, 2.3, 3.4, 4.5], 11.5)])
+    ([0.1, 1.2, 2.3, 3.4, 4.5], 11.5),
+    ([1,2,3,4], 10)])
 def test_mysum(numbers, output):
     assert mysum(numbers) == output
+    assert (len(numbers)%2) == 1
