522a3a0e52b69a7eec7a930ff07a410fb5d712a1
thib.chami@gmail.com <f92jL+T)2DiJ$6!q>
2021-01-13 09:24:50 +0100

    second exercice

--- sum_test.py
+++ sum_test.py
@@ -1,5 +1,7 @@
 import pytest
 from sum import mysum
+
+
 @pytest.mark.parametrize('numbers, output', [
     ([1, 2, 3], 6),
     ([1, 2, 3, 4, 5], 15),
@@ -10,5 +12,6 @@ from sum import mysum
 ])
 def test_mysum(numbers, output):
     assert mysum(numbers) == output
-    assert type(numbers[0]) == int
+    for i in range(len(numbers)):
+        assert type(numbers[i]) == int
     assert (len(numbers) % 2) == 1
