08c142979b2f1bcf1fc7570e0c32c9c57696e9d9
thib.chami@gmail.com <f92jL+T)2DiJ$6!q>
2021-01-13 09:22:30 +0100

    first exercice

--- sum_test.py
+++ sum_test.py
@@ -3,9 +3,9 @@ from sum import mysum
 @pytest.mark.parametrize('numbers, output', [
     ([1, 2, 3], 6),
     ([1, 2, 3, 4, 5], 15),
-    ([1, 2, 3, 4], 10),
-    ([1, 2, 3, 4, 5, 9], 24),
-    ([0, 27], 27)
+    ([0, 1, 2, 3, 4], 10),
+    ([0, 1, 2, 3, 4, 5, 9], 24),
+    ([0, 1, 26], 27)
     # Ajoutez des tests pour que la somme des éléments de la liste soit égale à 10, 24 et 27
 ])
 def test_mysum(numbers, output):
